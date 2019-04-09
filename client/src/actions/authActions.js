c39aea4c8a74b8171f9026a5377917afc49988d0
lasjorg <28780271+lasjorg@users.noreply.github.com>
2019-04-09 03:59:09 +0200

    fix: reorder methods

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { returnErrors } from './errorActions';
+
 import {
   USER_LOADED,
   USER_LOADING,
@@ -11,11 +12,13 @@ import {
   REGISTER_FAIL
 } from './types';
 
+// Check token & load user
 export const loadUser = () => (dispatch, getState) => {
+  // User loading
   dispatch({ type: USER_LOADING });
-  const config = tokenConfig(getState);
+
   axios
-    .get('/api/auth/user', config)
+    .get('/api/auth/user', tokenConfig(getState))
     .then(res =>
       dispatch({
         type: USER_LOADED,
@@ -30,19 +33,16 @@ export const loadUser = () => (dispatch, getState) => {
     });
 };
 
-export const logout = () => {
-  return {
-    type: LOGOUT_SUCCESS
-  }
-}
-
+// Register User
 export const register = ({ name, email, password }) => dispatch => {
+  // Headers
   const config = {
     headers: {
       'Content-Type': 'application/json'
     }
   };
 
+  // Request body
   const body = JSON.stringify({ name, email, password });
 
   axios
@@ -63,17 +63,59 @@ export const register = ({ name, email, password }) => dispatch => {
     });
 };
 
+// Login User
+export const login = ({ email, password }) => dispatch => {
+  // Headers
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  // Request body
+  const body = JSON.stringify({ email, password });
+
+  axios
+    .post('/api/auth', body, config)
+    .then(res =>
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      })
+    )
+    .catch(err => {
+      dispatch(
+        returnErrors(err.response.data, err.response.status, 'LOGIN_FAIL')
+      );
+      dispatch({
+        type: LOGIN_FAIL
+      });
+    });
+};
+
+// Logout User
+export const logout = () => {
+  return {
+    type: LOGOUT_SUCCESS
+  };
+};
+
+// Setup config/headers and token
 export const tokenConfig = getState => {
+  // Get token from localstorage
   const token = getState().auth.token;
 
+  // Headers
   const config = {
     headers: {
       'Content-type': 'application/json'
     }
   };
 
+  // If token, add to headers
   if (token) {
     config.headers['x-auth-token'] = token;
   }
+
   return config;
 };
